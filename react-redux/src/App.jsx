653ded06b297360b509a4cda776dde0c15b23d90
Sai Teja <saitejapubglite@gmail.com>
2024-03-04 09:22:16 +0530

    done

---
+++ react-redux/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Actions from './components/Action'
+import LikeCounter from './components/LikeCounter';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Actions />
+        <LikeCounter/>
+      </div>
+    </>
+  );
+}
+export default App
